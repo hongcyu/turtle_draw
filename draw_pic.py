91edc3ef14f43858b7ad62efc5093f84876d8958
hongCYu <41502466+hongcyu@users.noreply.github.com>
2020-08-16 12:39:00 +0800

    Update draw_pic.py

--- draw_pic.py
+++ draw_pic.py
@@ -7,7 +7,9 @@ def array_pic(pic):
     edges = cv2.Canny(image,100,200)
     img_gray = cv2.cvtColor(edges,cv2.COLOR_BAYER_BG2GRAY)
     ret, thresh = cv2.threshold(img_gray,127,255,0)
+    #下面这句是老版本opencv的写法，如果运行报错可以将下句注释，取消下下句的注释
     image,contours,hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    #contours,hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     xy = []
     for i in range(0,len(contours)):
         x,y,w,h = cv2.boundingRect(contours[i])
@@ -23,7 +25,7 @@ def array_pic(pic):
 def draw(xy):
     turtle.pensize(2)
     turtle.setup(width=0.6,height=1.0)
-    turtle.speed(1000)
+    turtle.speed(0)
     for array in xy[::-1]:
         turtle.penup()
         turtle.goto((array[0]/2)-100,-(array[1]/2)+250)
